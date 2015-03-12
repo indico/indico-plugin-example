02ec62ad6d5741f619768de70078468a14eeea01
Adrian Moennich <adrian.moennich@cern.ch>
2015-03-12 09:09:28 +0100

    Remove debug code

--- indico_example/static/js/example.js
+++ indico_example/static/js/example.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var $t = window.$t_example = $T.domain('example');
+    var $t = $T.domain('example');
     $('#js-msg').text($t.gettext('This message was generated using JavaScript'));
     $('#yes-msg').text($T.gettext('Edit'));
 });
