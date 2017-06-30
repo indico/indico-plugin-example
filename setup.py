79299ab327e4c991a40158c81093ed3521c8279c
Adrian Moennich <adrian.moennich@cern.ch>
2017-06-30 11:44:39 +0200

    Require indico 2.0, bump version to 1.0.dev0

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico_example',
-    version='0.4',
+    version='1.0.dev0',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
     install_requires=[
-        'indico>=1.9.10'
+        'indico>=2.0.dev0'
     ],
     entry_points={'indico.plugins': {'example = indico_example:ExamplePlugin'}}
 )
